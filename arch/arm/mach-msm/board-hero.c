2bbebc78783c442369ca6a8c115550f639d6e4cb
Behnam Fakhimi <behnam.fakhimi@gmail.com>
2010-05-21 20:02:35 +0200

    Speed is now stable, msm_acpu_clock_platform_data hero_clock_data was changed with right values

--- arch/arm/mach-msm/board-hero.c
+++ arch/arm/mach-msm/board-hero.c
@@ -1106,11 +1106,11 @@ static struct msm_acpu_clock_platform_data hero_clock_data = {
 	.acpu_switch_time_us = 20,
 	.max_speed_delta_khz = 256000,
 	.vdd_switch_time_us = 62,
-	.power_collapse_khz = 19200,
+	.power_collapse_khz = 19200000,
 #if defined(CONFIG_TURBO_MODE)
-	.wait_for_irq_khz = 176000,
+	.wait_for_irq_khz = 176000000,
 #else
-	.wait_for_irq_khz = 128000,
+	.wait_for_irq_khz = 128000000,
 #endif
 };
 
